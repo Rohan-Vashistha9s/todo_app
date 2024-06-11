57a39ddb936c0dcdb76e8fddf76c1cc73009ea96
rohan <panditrohan738@gmail.com>
2024-06-11 13:48:57 +0530

    first commit

---
+++ src/components/TodoList/TodoListItems.jsx
@@ -0,0 +1,25 @@
+import "./TodoListItems.css";
+import { useDispatch } from "react-redux";
+import { removeTodo } from "../../features/todoSlice";
+
+export default function TodoListItems(props) {
+  const dispatch = useDispatch();
+  return (
+    <div className="todoList">
+      <div className="todoDetail">
+        {/* all the data come from todoSlice file throught props */}
+        <span className="heading">{props.name}</span>
+        <span className="para">{props.description}</span>
+      </div>
+      <div className="button">
+        <button
+          onClick={() => {
+            dispatch(removeTodo(props.id));
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
